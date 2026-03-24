e544d23998a602987e3a16a36ab27dce517479c9
Giovanni Marigi <gio.marigi@gmail.com>
2026-03-24 07:59:01 +0100

    added offline libs

--- dist/install-libs.sh
+++ dist/install-libs.sh
@@ -1,3 +1,4 @@
 mvn install:install-file -Dfile=gpx.jar -DgroupId=com.topografix.gpx -DartifactId=gpxmodel -Dversion=1.0 -Dpackaging=jar
 mvn install:install-file -Dfile=gpx1.0.jar -DgroupId=com.topografix.gpx -DartifactId=gpxmodelx0 -Dversion=1.0 -Dpackaging=jar
 mvn install:install-file -Dfile=tcx.jar -DgroupId=com.garmin.xmlschemas.trainingCenterDatabase -DartifactId=tcxmodel -Dversion=1.0 -Dpackaging=jar
+mvn install:install-file -Dfile=JMapViewer.jar -DgroupId=org.openstreetmap.jmapviewer -DartifactId=jmapviewer -Dversion=1.18 -Dpackaging=jar
